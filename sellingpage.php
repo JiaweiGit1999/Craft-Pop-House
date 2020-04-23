ae60542767d91b80f703fec04fc66fda7a81a82e
JiaweiGit1999 <55531324+JiaweiGit1999@users.noreply.github.com>
2020-04-23 15:02:23 +0800

    Add files via upload
    Selling page + navigation bar

---
+++ sellingpage.php
@@ -0,0 +1,75 @@
+<?php echo '
+<!DOCTYPE html>
+<html>
+<title>Craft Pop House</title>
+<meta charset="UTF-8">
+
+<link rel="stylesheet" type="text/css"href="buyingpage.css">
+<body>
+
+<header >
+	<img src="pic/logo.png" alt="logo" id="logo">
+	<div id="title"> | Craft Pop House</div>
+	<form>
+		<label for="searchbar"></label>
+		<input type="text" id="searchbar" name="searchbar">
+		<input type="image" src="pic/searchlogo.png" alt="searchlogo" id="searchlogo">
+	</form>
+</header>
+<div>
+
+</div>';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dp2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT name, price, quantity,img FROM products";
+$result = $conn->query($sql);
+
+
+echo'<div id="sellersidenav">
+		<a class="sellermenu" href="sellingpage.php">Your products</a>
+		<a class="sellermenu" href="sellingpage.php">Your Sales</a>
+		<a class="sellermenu" href="sellingpage.php">Sold products</a>
+	</div>
+	<div class="topmenu">
+		<form id="sellermenuform">
+			<button type="submit" value="Remove">Remove</button>
+			<button type="submit" value="Add Product">Add product</button>
+		</form>
+	</div>';
+
+echo'<div id="productdisplay">';
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo '<div class="productcolumn"> 
+		<img src=' . $row["img"]. ' alt="testing" class="productimg">
+		<p class="productname">' . $row["name"] . '</p>
+		<p class="price"> RM' . $row["price"] . '</p>
+		<div class="stargroup">
+			<input type="image" src="pic/stars.png" alt="starslogo" class="starslogo">
+			<input type="image" src="pic/stars.png" alt="starslogo" class="starslogo">
+			<input type="image" src="pic/stars.png" alt="starslogo" class="starslogo">
+			<input type="image" src="pic/stars.png" alt="starslogo" class="starslogo">
+			<input type="image" src="pic/stars.png" alt="starslogo" class="starslogo">
+		</div>
+	</div>';
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+		
+echo'</div>
+</body>
+</html>';  ?>
