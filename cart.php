2795bf47a6fac265f4e9d212b1dcd4f252fa7cea
JasonThian <55531109+JasonThian@users.noreply.github.com>
2020-05-07 09:49:32 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -59,7 +59,6 @@ echo '
 	<div id="topheadnav">
 		<a href="sellingpage.php" id="sellingcentre">Seller Centre</a>
 		<div class="loginbox">';
-			session_start();
  
 	// Check if the user is already logged in, if yes then redirect him to welcome page
 	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
